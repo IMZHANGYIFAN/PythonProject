c616908473de0eab0c9f039ea2eac2075bcebc29
IMZHANGYIFAN <749867709@qq.com>
2018-01-28 01:05:17 +0800

    Half of the py file, cannot work yet

--- CheckCPU/CheckCPU.py
+++ CheckCPU/CheckCPU.py
@@ -1,2 +1,35 @@
 import psutil
+import os
+from influxdb import InfluxDBClient
+import time,math,random
+
+#get current pid
+p1 = psutil.Process(os.getpid())
+
+while True:
+    a = psutil.virtual_memory().percent
+
+    b = psutil.cpu_percent(interval=1.0)
+
+    json_body = [
+        {
+            "measurement": "cpu_load_short",
+            "tags": {
+                "host": "server01",
+                "region": "us-west"
+            },
+            "fields": {
+                "cpu": b,
+                "mem": a
+            }
+        }
+    ]
+
+    client = InfluxDBClient('localhost', 8086, 'root', 'root', "xxyyxx")
+    client.create_database('xxyyxx')
+    client.write_points(json_body)
+
+    result = client.query('select value from cpu_load_short;')
+    print("Result: {0}".format(result))
+    time.sleep(2)
 
